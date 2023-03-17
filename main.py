e24dba375a7bd3b3121df18375e90e8ce3ae417f
Денис <124888148+Ifpatr1ck@users.noreply.github.com>
2023-03-17 12:25:29 +0600

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import Parser
-url = 'https://omsk.hh.ru/search/vacancy?text=python&area=68'
-if __name__ == '__main__':
-    try:
-        Parser.parse(url)
-    except:
-        print("Ошибка")
-
-
